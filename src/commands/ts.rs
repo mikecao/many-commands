c49dbc543031ebc36ad81b35cd11d34dcb437b08
Mike Cao <mike@mikecao.com>
2026-07-23 23:52:10 -0700

    Rename ts flag to ms

--- src/commands/ts.rs
+++ src/commands/ts.rs
@@ -5,7 +5,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub struct TsArgs {
     /// Print the timestamp in milliseconds instead of seconds
     #[arg(long)]
-    pub millis: bool,
+    pub ms: bool,
 }
 
 pub fn run(args: TsArgs) {
@@ -13,7 +13,7 @@ pub fn run(args: TsArgs) {
         .duration_since(UNIX_EPOCH)
         .expect("system clock is before the unix epoch");
 
-    if args.millis {
+    if args.ms {
         println!("{}", now.as_millis());
     } else {
         println!("{}", now.as_secs());
